266a4d0c8dd6931d46a2e671f5af417475eb1fc0
VuTruongSon-05 <157568810+VuTruongSon-05@users.noreply.github.com>
2025-11-12 11:21:00 +0700

    feat: update layout common

---
+++ Pro219.Web/Constants/Router.cs
@@ -0,0 +1,13 @@
+﻿namespace Pro219.Web.Constants
+{
+    public static class RouterConst
+    {
+        public static class User
+        {
+            public const string Home = "/";
+            public const string SignUp = "/sign-up";
+            public const string Login = "/login";
+            public const string ForgotPassword = "/forgot-password";
+        }
+    }
+}
---
+++ Pro219.Web/Constants/constant.cs
@@ -0,0 +1,8 @@
+﻿namespace Pro219.Web.Constants
+{
+    public static class Constant
+    {
+        public const string PageTitleUser = "Adams Store";
+        public const string PageTitleAdmin = "Adams Store Management";
+    }
+}
