04669204fc12729e96854742ab27accf7699474d
VuTruongSon-05 <157568810+VuTruongSon-05@users.noreply.github.com>
2025-11-12 23:31:37 +0700

    feat: update active nav admin

--- Pro219.Web/Constants/Router.cs
+++ Pro219.Web/Constants/Router.cs
@@ -9,5 +9,16 @@
             public const string Login = "/login";
             public const string ForgotPassword = "/forgot-password";
         }
+
+        public static class Admin
+        {
+            public const string Home = "/admin/home";
+            public const string Login = "/admin/login";
+            public const string Product = "/admin/product";
+            public const string Brand = "/admin/brand";
+            public const string Category = "/admin/category";
+            public const string Bill = "/admin/bill";
+            public const string Statistical = "/admin/statistical";
+        }
     }
 }
