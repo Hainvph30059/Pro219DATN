a55e4a58e516edfedff454a8938df6c60b21f56d
NguyenPH55325 <157569137+NguyenPH55325@users.noreply.github.com>
2025-11-14 09:07:45 +0700

    add ultilities file to store common method

---
+++ Pro219.BUS/Utilities/UtilityFunc.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Pro219.API.Utilities
+{
+    public class UtilityFunc
+    {
+        public string HashPassword(string password)
+        {
+            //4297F44B13955235245B2497399D7A93 
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(password);
+            byte[] hash = md5.ComputeHash(inputBytes);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            md5.Clear();
+            return sb.ToString();
+
+        }
+    }
+}
