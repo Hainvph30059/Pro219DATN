f7b5fc4f0ae561b2e0736664eb0caae4da560cf5
VuTruongSon-05 <157568810+VuTruongSon-05@users.noreply.github.com>
2025-11-15 02:46:02 +0700

    feat: example validate and UI form

--- Pro219.Web/Constants/constant.cs
+++ Pro219.Web/Constants/constant.cs
@@ -11,6 +11,7 @@
         public class Regex
         {
             public const string Password = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9]).{8,16}$";
+            public const string PhoneNumber = @"(03|05|07|08|09|01[2|6|8|9])+([0-9]{8})\b";
         }
 
         public class MessageValid
@@ -18,7 +19,12 @@
             public const string Password = "Mật khẩu phải có từ 8 đến 16 ký tự chữ và số, bao gồm cả chữ hoa, chữ thường, số và ký hiệu.";
             public const string Required = "Không được để trống.";
             public const string Max255 = "Tối đa 255 kí tự.";
+            public const string Max200 = "Tối đa 200 kí tự.";
+            public const string Max100 = "Tối đa 100 kí tự.";
+            public const string Min2 = "Tối thiểu 2 kí tự.";
             public const string Email = "Sai định dạng Email.";
+            public const string PhoneNumber = "Sai định dạng số điện thoại.";
+            public const string PhoneNumberLength = "Tối thiểu 10 số và tối đa 11 số";
         }
     }
 }
---
+++ Pro219.Web/Schema/CustomerSchema.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Pro219.Web.Constants;
+
+namespace Pro219.Web.Schema
+{
+    public class CustomerSchema
+    {
+        public class Create
+        {
+            [Required(ErrorMessage = Constant.MessageValid.Required)]
+            [MaxLength(200, ErrorMessage = Constant.MessageValid.Max200)]
+            [MinLength(2, ErrorMessage = Constant.MessageValid.Min2)]
+            public string FullName { get; set; } = string.Empty;
+
+            [MaxLength(11, ErrorMessage = Constant.MessageValid.PhoneNumberLength)]
+            [MinLength(10, ErrorMessage = Constant.MessageValid.PhoneNumberLength)]
+            [RegularExpression(Constant.Regex.PhoneNumber, ErrorMessage = Constant.MessageValid.PhoneNumber)]
+            public string PhoneNumber { get; set; }
+
+            [Required(ErrorMessage = Constant.MessageValid.Required)]
+            [MaxLength(100, ErrorMessage = Constant.MessageValid.Max100)]
+            [EmailAddress(ErrorMessage = Constant.MessageValid.Email)]
+            public string Email { get; set; }
+
+            public DateTime? DateOfBirth { get; set; }
+
+            [Required(ErrorMessage = Constant.MessageValid.Required)]
+            [MaxLength(16, ErrorMessage = Constant.MessageValid.Password)]
+            [MinLength(8, ErrorMessage = Constant.MessageValid.Password)]
+            [RegularExpression(Constant.Regex.Password, ErrorMessage = Constant.MessageValid.Password)]
+            public string PasswordHash { get; set; } = string.Empty;
+
+            [Required(ErrorMessage = Constant.MessageValid.Required)]
+            public byte StatusByte { get; set; }
+        }
+    }
+}
