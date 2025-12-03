c2fc343f272b78cf9a6755c7658bf18f9f05094b
haitmph33636-cyber <haitmph33636@fpt.edu.vn>
2025-12-03 20:45:50 +0700

    add feat to caculate fee for shipping

---
+++ Pro219.BUS/DTOs/FeeItemDTO.cs
@@ -0,0 +1,14 @@
+namespace Pro219.API.DTOs
+{
+    public class FeeItemDTO
+    {
+        public string Name { get; set; } = "Quần áo";
+        public int Quantity { get; set; } = 1;
+        public int Length { get; set; } = 35;
+        public int Width { get; set; } = 35;
+        public int Height { get; set; } = 5;
+        public int Weight { get; set; } =500;
+    }
+}
+
+
