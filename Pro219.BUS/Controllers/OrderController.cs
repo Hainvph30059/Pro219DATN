61391ee6b284b2ac87dbeeda101db0fc3d94b08c
haitmph33636-cyber <haitmph33636@fpt.edu.vn>
2025-12-05 16:13:13 +0700

    add feature to get url checkout, webhook catch data from payos

--- Pro219.BUS/Controllers/OrderController.cs
+++ Pro219.BUS/Controllers/OrderController.cs
@@ -10,6 +10,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Net.payOS.Types;
+using Net.payOS;
 
 namespace Pro219.API.Controllers
 {
@@ -24,6 +26,170 @@ namespace Pro219.API.Controllers
             orderRepository = new OrderRepository();
         }
 
+       [HttpPost("GetCheckoutUrl")]
+       public async Task<ActionResult<string>> GetCheckoutUrl([FromBody] List<CheckoutItemDTO> listProduct, string discountCode = null)
+       {
+
+         
+
+
+            PayOS payOS = new PayOS("09b8a42b-6105-4cd4-a4ee-8492e42e909c", "15cfbaf8-79a4-48a0-908f-248c30538001", "00b20c6b94e21bf27e6cb0ae2f26515637c93d70b2eeb832e7b51e299cba433d");
+            List<ItemData> items = new List<ItemData>();
+            foreach (var product in listProduct)
+            {
+                ItemData item = new ItemData(product.ProductName, product.Quantity, (int)product.UnitPrice);
+                items.Add(item);
+            }
+
+            decimal totalPrice = listProduct.Sum(p => p.UnitPrice * p.Quantity);
+            decimal discountAmount = 0;
+            if (discountCode != null)
+            {
+                DiscountCodeRepository discountRepository = new DiscountCodeRepository();
+                var discount = await discountRepository.GetDiscountCodeByCode(discountCode);
+                if (discount != null)
+                {
+
+                    if (discount.DiscountType == "Percentage")
+                    {
+                        discountAmount = totalPrice * (discount.Value / 100);
+                        totalPrice -= discountAmount;
+                        discountAmount = discountAmount;
+                    }
+                    else if (discount.DiscountType == "FixedAmount")
+                    {
+                        discountAmount = discount.Value;
+                        totalPrice -= discountAmount;
+                        discountAmount = discountAmount;
+                    }
+                }
+            }
+            decimal finalAmount = totalPrice - discountAmount;
+            int ordCode = new Random().Next(1, int.MaxValue);
+            Order order = new Order();
+            try
+            {
+                order.OrderCode = "DH" + ordCode.ToString();
+                order.TotalAmount = finalAmount;
+                order.DiscountAmount = discountAmount;
+                order.FinalAmount = finalAmount;
+                order.PaymentStatus = "Pending";
+                order.OrderStatus = "Pending";
+                order.Notes = "Đang chờ thanh toán qua PayOS";
+                order.CreateAt = DateTime.Now;
+                order.LastUpdate = DateTime.Now;
+                order.UpdateBy = "System";
+                order.Status = 1;
+                order.CustomerId = int.Parse(User.FindFirst(ClaimTypes.SerialNumber)?.Value);
+                order.ShippingAddressId = 1;
+                order.DiscountId = null;
+                order.PaymentMethodId = 1;
+                var result = await orderRepository.AddOrder(order);
+
+
+                if (result == null)
+                {
+                    return BadRequest();
+                }
+                else
+                {
+                    OrderItemRepository orderItemRepository = new OrderItemRepository();
+                    foreach (var product in listProduct)
+                    {
+                        OrderItem orderItem = new OrderItem();
+                        orderItem.OrderId = result.OrderId;
+                        orderItem.ProductVariantId = product.ProductVariantId;
+                        orderItem.Quantity = product.Quantity;
+                        orderItem.UnitPrice = product.UnitPrice;
+                        orderItem.Subtotal = product.UnitPrice * product.Quantity;
+                        orderItem.Delete = false;
+                        var resultItem = await orderItemRepository.AddOrderItem(orderItem);
+                        if(resultItem == null)
+                        {
+                            return BadRequest();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, Constant.ErrorCode.OtherError);
+            }
+            PaymentData paymentData = new PaymentData(ordCode, (int)finalAmount, "Adam Store Thanh toán", items, "https://localhost:7179/Order/PaymentCanceled?orderId=" + order.OrderId + "&errorMessage=" + "Đã hủy thanh toán", "https://localhost:7179/Order/PaymentSuccess?orderId=" + order.OrderId );
+
+            CreatePaymentResult createPayment = await payOS.createPaymentLink(paymentData);
+            
+            if(createPayment.status == "PENDING")
+            {    
+              
+                return Ok(createPayment.checkoutUrl);
+            }
+            else
+            {
+                return BadRequest();
+            }           
+           
+
+        }
+
+        [HttpGet("PaymentSuccess")]
+        public async Task<ActionResult<Order>> PaymentSuccess( [FromQuery] int orderId, [FromQuery] string errorMessage = null)
+        {
+
+            try
+            {
+                var order = await orderRepository.GetOrderById(orderId);
+                if(order == null)
+                {
+                    return NotFound();
+                }
+                order.PaymentStatus = "Đã thanh toán";
+                order.OrderStatus = "Đã thanh toán";
+                order.LastUpdate = DateTime.Now;
+                order.UpdateBy = "System";
+                var result = await orderRepository.UpdateOrder(order);
+                if(result == null)
+                {
+                    return BadRequest();
+                }
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, Constant.ErrorCode.OtherError);
+            }
+
+        }
+
+        [HttpGet("PaymentCanceled")]
+        public async Task<ActionResult<Order>> PaymentCanceled([FromQuery] int orderId, [FromQuery] string errorMessage = null)
+        {
+            try
+            {
+                var order = await orderRepository.GetOrderById(orderId);
+                if(order == null)
+                {
+                    return NotFound();
+                }
+                order.PaymentStatus = "Canceled";
+                order.OrderStatus = "Đã hủy";
+                order.LastUpdate = DateTime.Now;
+                order.UpdateBy = "System";
+                var result = await orderRepository.UpdateOrder(order);
+                if(result == null)
+                {
+                    return BadRequest();
+                }
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, Constant.ErrorCode.OtherError);
+            }
+        }
+
+
+      
         [HttpGet("GetAll")]
         public async Task<ActionResult<List<Order>>> GetAllOrders()
         {
